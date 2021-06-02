4d192de5c20e37db5b34c9addb4b4cd9b250ef09
elecodev <eric.leconte@gmail.com>
2021-06-02 06:41:13 +0100

    .

--- main.py
+++ main.py
@@ -135,27 +135,27 @@ trendline_support= ['f','sh_avgvol_o500,sh_short_o5,ta_changeopen_u,ta_pattern_t
 
 stocks_downgraded_on_up = build(downgraded_on_up)
 ddiff = diff(stocks_downgraded_on_up, deserialized_downgraded_on_up)
-print("diff downgraded:" + str(ddiff))
+print("diff downgraded:" + list(ddiff))
  
 stocks_breakout = build(breakout)
 ddiff = diff(stocks_breakout, deserialized_breakout)
-print("diff breakout:" + str(ddiff))
+print("diff breakout:" + list(ddiff))
 
 stocks_low_pe = build(low_pe)
 ddiff = diff(stocks_low_pe, deserialized_low_pe)
-print("diff low pe:" + str(ddiff))
+print("diff low pe:" + list(ddiff))
 
 stocks_canslim = build(canslim)
 ddiff = diff(stocks_canslim, deserialized_canslim)
-print("diff cansli,:" + str(ddiff))
+print("diff cansli,:" + list(ddiff))
 
 stocks_trend_hammer = build(trend_and_hammer)
 ddiff = diff(stocks_trend_hammer, deserialized_hammer)
-print("diff hammer:" + str(ddiff))
+print("diff hammer:" + list(ddiff))
 
 stocks_trendline = build(trendline_support)
 ddiff = diff(stocks_trendline, deserialized_trendline)
-print("diff trendline:" + str(ddiff))
+print("diff trendline:" + list(ddiff))
 
 
 #Write to S3 using unique key - EmpId007
